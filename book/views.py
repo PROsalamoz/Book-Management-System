ac909b10e92240c0de7dc9a7862b759b1edcf660
mahmoud_salama <salamozm346@gmail.com>
2021-05-25 03:02:11 +0200

    handle books page and images

--- book/views.py
+++ book/views.py
@@ -13,4 +13,10 @@ def index(request):
 
 
 def books(request):
-    return render(request, 'pages/book.html')
+    book = Book.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'books': book,
+        'categories': categories
+    }
+    return render(request, 'pages/book.html', context)
